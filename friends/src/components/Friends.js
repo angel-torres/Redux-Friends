c4bd3a5d5698d0290e52dfffb6f1079c387f921c
Angel Torres <alas4224@gmail.com>
2019-01-30 22:31:59 -0600

    finished styling friend cards

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -11,7 +11,15 @@ class Friends extends Component {
         return (
         <div>
             { this.props.fetchingFriends && (<Loader type="Oval" color="black" height="90" width="60" />)}
-            { this.props.friendsFetched && this.props.friends.map( friend => (<h1 key={friend.id}>{friend.name}</h1>))}
+            <div style={{display:"flex", width:"800px", flexWrap:"wrap", margin:"auto", justifyContent:"center"}}>
+                { this.props.friendsFetched && this.props.friends.map( friend => (
+                    <div key={friend.id} className="card text-white bg-dark mb-3" style={{margin:"20px", width:"25%"}}>
+                        <h1 className="card-header">{friend.name}</h1>
+                        <p className="card-title">{friend.email}</p>
+                        <p className="card-title">{friend.age}</p>
+                    </div>
+                ))}
+            </div>
         </div>
         )
     }
